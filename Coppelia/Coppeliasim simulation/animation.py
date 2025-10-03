90ac77cdcf73e7908f76f4cdfd3e7bff6e6ff7c4
takosenn <k954618@kansai-u.ac.jp>
2025-10-03 14:24:52 +0900

    commit

--- Coppelia/Coppeliasim simulation/animation.py
+++ Coppelia/Coppeliasim simulation/animation.py
@@ -15,15 +15,22 @@ class Animation:
         self.agent_positions = []
         for i in range(num_agents):
             theta = 2 * np.pi * i / num_agents
-            agent_position = [center[0] + 5 * np.cos(theta) , center[1] + radius + 5 * np.sin(theta) , 2]
+            agent_position = [center[0] + 5 * np.cos(theta) , center[1] + radius + 5 * np.sin(theta) , 2]           #要素3(x,y,z)
             self.agent_positions.append(agent_position)
             self.sim.initial_setAgentpositions(i, agent_position)
-        self.prev_agent_positions = self.agent_positions
+        self.prev_agent_positions = []
+        for i in range(num_agents):
+            theta = 2 * np.pi * i / num_agents
+            prev_agent_position = [center[0] + 5 * np.cos(theta) , center[1] + radius + 5 * np.sin(theta) , 2]           #要素3(x,y,z)
+            self.prev_agent_positions.append(prev_agent_position)
         self.target_position = [0 , 5 ,2]
         self.sim.initial_settargetposition(self.target_position)
         self.prev_target_position = []
         self.ro_i = []
         self.coordinates = []
+        self.theta = np.zeros(num_agents)
+        self.theta_plus = np.zeros(num_agents)
+        self.theta_minus = np.zeros(num_agents)
         self.prev_theta = np.zeros(num_agents)
         self.prev_theta_plus = np.zeros(num_agents)
         self.prev_theta_minus =np.zeros(num_agents)
@@ -42,35 +49,33 @@ class Animation:
         j_minus = (i-1) % num_agents
 
         theta_target = omega_target * i
-        self.target_position = [center[0]  + radius * np.sin(theta_target) , center[1]  + radius * np.cos(theta_target) , 2]
-
-        vec = np.array(self.agent_positions[j]) - np.array(self.target_position)                    
-        vec_plus = np.array(self.agent_positions[j_plus]) - np.array(self.target_position)          
-        vec_minus = np.array(self.agent_positions[j_minus]) - np.array(self.target_position)        
-        theta = np.arctan2(vec[1] , vec[0])                                                         
-        theta_plus = np.arctan2(vec_plus[1] , vec_plus[0])
-        print(theta_plus)
-        theta_minus = np.arctan2(vec_minus[1] , vec_minus[0])
+        self.target_position = [center[0]  + radius * np.sin(theta_target) , center[1]  + radius * np.cos(theta_target) , 2]            #要素3(x,y,z)
+        vec = np.array(self.agent_positions[j]) - np.array(self.target_position)                                                        #要素3(x,y,z)同士の引き算
+        vec_plus = np.array(self.agent_positions[j_plus]) - np.array(self.target_position)                                              #要素3(x,y,z)同士の引き算
+        vec_minus = np.array(self.agent_positions[j_minus]) - np.array(self.target_position)                                            #要素3(x,y,z)同士の引き算
+        self.theta = np.arctan2(vec[1] , vec[0])                                                                                    #targetの位置更新がAgentより多いせいで変わる(もしかしたら)                                                                                                                   
+        self.theta_plus = np.arctan2(vec_plus[1] , vec_plus[0])
+        self.theta_minus = np.arctan2(vec_minus[1] , vec_minus[0])
 
         self.ro_i = np.linalg.norm(vec)
         #角速度の計算
-        self.omega_i = theta - self.prev_theta[j]
-        self.omega_i_plus = theta_plus - self.prev_theta_plus[j]
-        self.omega_i_minus = theta_minus - self.prev_theta_minus[j]
+        self.omega_i = (self.theta - self.prev_theta[j]) / delta_time
+        self.omega_i_plus = (self.theta_plus - self.prev_theta_plus[j]) / delta_time
+        self.omega_i_minus = (self.theta_minus - self.prev_theta_minus[j]) / delta_time
         
         self.omega_i = (self.omega_i + np.pi) % (2 * np.pi) - np.pi
         self.omega_i_plus = (self.omega_i_plus + np.pi) % (2 * np.pi) - np.pi
         self.omega_i_minus = (self.omega_i_minus + np.pi) % (2 * np.pi) - np.pi
 
-
         #前回のthetaを保持
-        self.prev_theta[j] = theta
-        self.prev_theta_plus[j] = theta_plus
-        self.prev_theta_minus[j] = theta_minus
+        self.prev_theta[j] = np.copy(self.theta)
+        self.prev_theta_plus[j] = np.copy(self.theta_plus)
+        self.prev_theta_minus[j] = np.copy(self.theta_minus)
+
         #agent間の角距離計算
-        diff = abs(theta - theta_plus)
+        diff = abs(self.theta - self.theta_plus)
         self.alpha_i = min(diff, 2 * np.pi - diff)
-        diff_minus = abs(theta - theta_minus)
+        diff_minus = abs(self.theta - self.theta_minus)
         self.alpha_i_minus = min(diff_minus, 2 * np.pi - diff_minus)
 
         # 放射方向・接線方向の単位ベクトル
@@ -80,8 +85,8 @@ class Animation:
         # targetの速度ベクトルを計算
         target_velocity = np.array(
             [
-                -radius * omega_target * np.cos(theta),  # x方向の速度成分
-                -radius * omega_target * np.sin(theta),  # y方向の速度成分
+                -radius * omega_target * np.cos(self.theta),  # x方向の速度成分
+                -radius * omega_target * np.sin(self.theta),  # y方向の速度成分
                 0
             ]
         )
@@ -89,7 +94,6 @@ class Animation:
 
         # エージェントの速度ベクトルを計算(現在の位置と前の位置から)
         agent_velocity = (np.array(self.agent_positions[j]) - np.array(self.prev_agent_positions[j])) / delta_time
-        self.prev_time = current_time
 
         # 相対速度の計算
         relative_velocity = agent_velocity - target_velocity
@@ -126,8 +130,8 @@ class Animation:
         #    writer.writerow([result[0], result[1], ro_i, agent_positions[0][1] , agent_positions[1][1] , agent_positions[2][1] , agent_positions[3][1] , agent_positions[4][1] , agent_positions[5][1]])     # データをCSVに書き込む
 
         # 前の位置を更新
-        self.prev_agent_positions[j] = self.agent_positions[j]
-        self.prev_target_position = self.target_position
+        self.prev_agent_positions[j] = np.copy(self.agent_positions[j])                     #きちんとコピーできていなかったが、np.copyでできた
+        self.prev_target_position = np.copy(self.target_position)
 
         # Coppeliasim側でAgentの緑の球(target)の位置同期
         for j in range(num_agents):
@@ -140,3 +144,4 @@ class Animation:
             self.sim.setAgentposition(j, Agents_pos_3d)
         target_pos_3d = [float(self.target_position[0]), float(self.target_position[1]), 2.0]
         self.sim.settargetposition(target_pos_3d)
+        self.prev_time = current_time
